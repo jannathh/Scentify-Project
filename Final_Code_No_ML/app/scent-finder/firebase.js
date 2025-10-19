e2b1bdf79cb6d027451d20709b5a6c46d006c453
jannathh <muheenjannath@gmail.com>
2025-10-19 19:43:55 +0400

    removed another firebase part

--- Final_Code_No_ML/app/scent-finder/firebase.js
+++ Final_Code_No_ML/app/scent-finder/firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database"; // For Realtime Database
 import { getFirestore } from "firebase/firestore"; // For Firestore
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDs4_FcOYZOtHcssfRpLur5j6R9EQJttTw",
-  authDomain: "scentify-54c2e.firebaseapp.com",
-  projectId: "scentify-54c2e", // ✅ cleaned
-  databaseURL: "https://scentify-54c2e-default-rtdb.europe-west1.firebasedatabase.app",
-  storageBucket: "scentify-54c2e.appspot.com", // ✅ fixed domain
-  messagingSenderId: "964174847844",
-  appId: "1:964174847844:web:0927e6e07d05324ba26471" // ✅ cleaned
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDs4_FcOYZOtHcssfRpLur5j6R9EQJttTw",
+//   authDomain: "scentify-54c2e.firebaseapp.com",
+//   projectId: "scentify-54c2e", // ✅ cleaned
+//   databaseURL: "https://scentify-54c2e-default-rtdb.europe-west1.firebasedatabase.app",
+//   storageBucket: "scentify-54c2e.appspot.com", // ✅ fixed domain
+//   messagingSenderId: "964174847844",
+//   appId: "1:964174847844:web:0927e6e07d05324ba26471" // ✅ cleaned
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
